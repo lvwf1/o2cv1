507bd9566a2a68436b05f2535342f06256ffc230
bruce <bruce@cloud-88.com>
2017-03-08 01:45:21 -0500

    Add Login/Counts/Criteria view pages and the CSS/js files to the project. Add LoginViewModel and AccessController class. Now user can sign on from Login page and see the other pages. Pages order: Login->Counts(Click "View")->Criteria

---
+++ O2CV1Solution/Controllers/AccessController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data;
+using O2V1Web.Models.ViewModels;
+using O2.Web.Context;
+
+namespace O2V1Web.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel loginview)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CountsContext Contsctx = new CountsContext())
+                {
+                    String userpass = Contsctx.Encrypt(loginview.Password);
+                    var v = Contsctx.CountUsers.Where(a => a.Usermail.Equals(loginview.Username) && a.UserPassword.Equals(userpass)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(v.UserName, false);
+                        Session["LogedUserID"] = v.CountUserId.ToString();
+                        Session["LogedUserFullname"] = v.UserName.ToString();
+
+                        return RedirectToAction("Mycount", "Counts");
+                    }
+                    else
+                    {
+                        ViewBag.LoginError = "Invalid Username or Password";
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginCos(LoginViewModel loginview)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CountsContext Contsctx = new CountsContext())
+                {
+                    String userpass = Contsctx.Encrypt(loginview.Password);
+                    var v = Contsctx.CountUsers.Where(a => a.Usermail.Equals(loginview.Username) && a.UserPassword.Equals(userpass)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(v.UserName, false);
+                        Session["LogedCosUserID"] = v.CountUserId.ToString();
+                        Session["LogedCosUserFullname"] = v.UserName.ToString();
+                        Session["CosDataSet"] = new DataSet();
+                        return RedirectToAction("Counts", "Home");
+
+                    }
+                    else
+                    {
+                        ViewBag.LoginError = "Invalid Username or Password";
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            FormsAuthentication.RedirectToLoginPage();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
